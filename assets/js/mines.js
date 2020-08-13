8ec7d307e94564d19b2636efd9173fa1f7e0a62e
NguyenKhoa <30405136+hp09com@users.noreply.github.com>
2020-08-13 08:38:07 +0700

    City Game Z (X,Y)

--- assets/js/mines.js
+++ assets/js/mines.js
@@ -2,7 +2,12 @@ const mcFarm = $(".mc-farm")
 const total = mcFarm.length//Count class exist
 const widthMine = 3500
 const heightMine = 3827
+const heightPage = $(document).height()
+const widthPage = $(document).height()
+var scrollWidth = widthPage-2550
+var scrollHeight = heightPage-2300
 var x = y = 0
+
 for (let index = 1; index <= total; index++) {
     x = Math.floor(Math.random() * widthMine)
     y = Math.floor(Math.random() * heightMine)
@@ -13,6 +18,7 @@ for (let index = 1; index <= total; index++) {
     elements.css("top", y)
     //.attr("sort-id", "x: "+x+"y: "+y)
 }
+
 function locateX(x, widthMine)
 {
     if(x < 1100 || x > 1400) 
@@ -22,6 +28,7 @@ function locateX(x, widthMine)
     x = Math.floor(Math.random() * widthMine)
     locateX(x, widthMine)
 }
+
 function locateY(y, heightMine)
 {
     if(y < 1800 || y > 2200)
@@ -31,6 +38,32 @@ function locateY(y, heightMine)
     y = Math.floor(Math.random() * heightMine)
     locateY(y, heightMine)
 }
+
+function moveScreenX(x, scrollWidth)
+{
+    if(x > 1420)
+    { return scrollWidth = scrollWidth + 100 }
+    if(x < 50)
+    {return scrollWidth = scrollWidth - 100 }
+}
+
+$(document).ready( function() {
+    console.log("hey")
+    $('html, body').animate({scrollLeft: scrollWidth }, 0)
+    
+    $("html, body").animate({ scrollTop: scrollHeight }, 0)
+})
+
+$("body").mousemove( (e) => {
+    var x = e.clientX
+    var y = e.clientY
+    console.log(x, y, scrollWidth)
+    x = moveScreenX(x, scrollWidth)
+    //$('html, body').animate({scrollLeft: x }, 0)
+    console.log(x)
+    //y = moveScreenY(y)
+})
+
 $(document).ready( () => {
     $(".mc-farm[data-src='false']").on("click", function(e){
         const id = $(this).data('sortid')
